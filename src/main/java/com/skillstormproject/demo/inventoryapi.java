a8ac3a76d27a1f572d6efc85b77395229f4727c1
sonialinse7 <sonialinse7@gmail.com>
2024-05-01 17:36:21 -0400

    first commit

---
+++ src/main/java/com/skillstormproject/demo/inventoryapi.java
@@ -0,0 +1,13 @@
+package com.skillstormproject.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class inventoryapi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(inventoryapi.class, args);
+	}
+
+}
