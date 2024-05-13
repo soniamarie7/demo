5d1d6939a589c6c172016fc72433356f56475ce3
sonialinse7 <sonialinse7@gmail.com>
2024-05-13 11:55:30 -0400

    ...

--- src/main/java/com/skillstormproject/demo/models/Product.java
+++ src/main/java/com/skillstormproject/demo/models/Product.java
@@ -1,13 +1,39 @@
 package com.skillstormproject.demo.models;
 
 public class Product {
-    String array[] = { "Catfood", "Dogfood", "Sciencediet", "Arcana", "Stellaandchewy" };
+private int idproduct;
+private String type;
+private String brand;
+private int warehouseId;
+public int getIdproduct() {
+    return idproduct;
+}
+public void setIdproduct(int idproduct) {
+    this.idproduct = idproduct;
+}
+public String getType() {
+    return type;
+}
+public void setType(String type) {
+    this.type = type;
+}
+public String getBrand() {
+    return brand;
+}
+public void setBrand(String brand) {
+    this.brand = brand;
+}
+public int getWarehouseId() {
+    return warehouseId;
+}
+public void setWarehouseId(int warehouseId) {
+    this.warehouseId = warehouseId;
+}
+@Override
+public String toString() {
+    return "Product [idproduct=" + idproduct + ", type=" + type + ", brand=" + brand + ", warehouseId=" + warehouseId
+            + "]";
+}
 
-    public String[] getArray() {
-        return array;
-    }
 
-    public void setArray(String[] array) {
-        this.array = array;
-    }
 }
